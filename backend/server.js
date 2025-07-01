742383b0ea6adbb2ca2c151365e325f123056296
Atharv Patil <atharvpatil5177@gmail.com>
2025-07-01 23:58:35 +0530

    project structure & server updated

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,27 @@
-import express from "express";
-import dotenv from "dotenv";
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
 
 dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
-});
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`app listening on port ${process.env.PORT || 7000}`);
-});
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+// Connect DB and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`),
+    );
+  })
+  .catch((err) => console.error(err));
