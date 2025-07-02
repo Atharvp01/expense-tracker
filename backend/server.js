e1406ee5aab5c8a4380e9c2427feb240446c668e
Atharv Patil <atharvpatil5177@gmail.com>
2025-07-02 13:15:44 +0530

    deployment variables updated

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["https://expense-tracker-api-pearl.vercel.app"],
+    origin: ["https://expense-tracker-xi-fawn.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   }),
